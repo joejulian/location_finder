bb8036ed041b08b7a02d2d06050015f5de5a474a
jjulian <jjulian@gaealink.net>
2010-01-25 10:47:59 -0800

    Changed DOMDocument handling to work with php4 > 4.2.0

--- site/phpsqlsearch_genxml.php
+++ site/phpsqlsearch_genxml.php
@@ -18,7 +18,11 @@
 * See COPYRIGHT.php for copyright notices and details.
 */
 
+header("Content-type: text/xml");
+// ini_set("display_errors", false);
+
 require("../../configuration.php");
+
 // Fixes the encoding to uf8
 function fixEncoding($in_str)
 {
@@ -36,10 +40,29 @@ $center_lat = $_GET["lat"];
 $center_lng = $_GET["lng"];
 $radius = $_GET["radius"];
 
+// PHP_VERSION_ID is available as of PHP 5.2.7, if our version is lower than that, then emulate it
+if (!defined('PHP_VERSION_ID')) {
+    $version = explode('.', PHP_VERSION);
+
+    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+}
+
 // Start XML file, create parent node
-$dom = new DOMDocument('<?xml version="1.0" encoding="utf-8"?><root/>');
-$node = $dom->createElement("markers");
-$parnode = $dom->appendChild($node);
+if( ( 40200 <= PHP_VERSION_ID ) && ( PHP_VERSION_ID < 50000 ) ) {
+    $dom = domxml_new_doc("1.0");
+    $dom_createelement_model = "create_element";
+    $dom_appendchild_model = "append_child";
+    $dom_setattribute_model = "set_attribute";
+} elseif (PHP_VERSION_ID >= 50000) {
+    $dom = new DOMDocument('<?xml version="1.0" encoding="utf-8"?><root/>');
+    $dom_createelement_model = "createElement";
+    $dom_appendchild_model = "appendChild";
+    $dom_setattribute_model = "setAttribute";
+} else {
+    die("<b>Error: </b>PHP Version not supported");
+}
+$node = $dom->$dom_createelement_model("markers");
+$parnode = $dom->$dom_appendchild_model($node);
 
 // Opens a connection to a mySQL server
 $connection=mysql_connect ($config->host, $config->user, $config->password);
@@ -79,25 +102,28 @@ if (!$result) {
   die("Invalid query: " . mysql_error());
 }
 
-header("Content-type: text/xml");
-
 // Iterate through the rows, adding XML nodes for each
 while ($row = @mysql_fetch_assoc($result)){
-  $node = $dom->createElement("marker");
-  $newnode = $parnode->appendChild($node);
-  $newnode->setAttribute("name", fixEncoding($row['name']));
-  $newnode->setAttribute("address", fixEncoding($row['address']));
-  $newnode->setAttribute("addr1",fixEncoding($row['addr1']));
-  $newnode->setAttribute("addr2",fixEncoding($row['addr2']));
-  $newnode->setAttribute("country",fixEncoding($row['country']));
-  $newnode->setAttribute("phone",fixEncoding($row['phone']));
-  $newnode->setAttribute("email",fixEncoding($row['email']));
-  $newnode->setAttribute("url",fixEncoding($row['url']));
-  $newnode->setAttribute("desc",fixEncoding($row['description']));
-  $newnode->setAttribute("lat", fixEncoding($row['lat']));
-  $newnode->setAttribute("lng", fixEncoding($row['lng']));
-  $newnode->setAttribute("distance", fixEncoding($row['distance']));
+  $node = $dom->$dom_createelement_model("marker");
+  $newnode = $parnode->$dom_appendchild_model($node);
+  $newnode->$dom_setattribute_model("name", fixEncoding($row['name']));
+  $newnode->$dom_setattribute_model("address", fixEncoding($row['address']));
+  $newnode->$dom_setattribute_model("addr1",fixEncoding($row['addr1']));
+  $newnode->$dom_setattribute_model("addr2",fixEncoding($row['addr2']));
+  $newnode->$dom_setattribute_model("country",fixEncoding($row['country']));
+  $newnode->$dom_setattribute_model("phone",fixEncoding($row['phone']));
+  $newnode->$dom_setattribute_model("email",fixEncoding($row['email']));
+  $newnode->$dom_setattribute_model("url",fixEncoding($row['url']));
+  $newnode->$dom_setattribute_model("desc",fixEncoding($row['description']));
+  $newnode->$dom_setattribute_model("lat", fixEncoding($row['lat']));
+  $newnode->$dom_setattribute_model("lng", fixEncoding($row['lng']));
+  $newnode->$dom_setattribute_model("distance", fixEncoding($row['distance']));
 }
 
-echo $dom->saveXML($dom->documentElement);
+if( ( PHP_VERSION_ID < 50000 ) ) {
+    $xmlfile = $doc->dump_mem();
+    echo $xmlfile;
+} else {
+    echo $dom->saveXML($dom->documentElement);
+}
 ?>
